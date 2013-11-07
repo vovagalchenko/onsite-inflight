f76a3f5cb09ee777f97de8a319de289be6d278c7
Vova Galchenko <vova@galchenko.com>
2013-11-07 13:26:10 -0800

    Improved usability of the expenses UI

--- webapp/js/expenses.js
+++ webapp/js/expenses.js
@@ -40,17 +40,11 @@ function initialize_ui(data)
     });
     build_content_from_template("handlebars-template", data, "expenses-form");
 
-    var fields = document.getElementsByClassName("field");
-    for (var i = 0; i < fields.length; i++)
-    {
-        fields[i].addEventListener("mouseout", field_mouseout, true);
-    }
-
     var inputs = document.getElementsByTagName("input");
-    for (var i = 0; i < fields.length; i++)
+    for (var i = 0; i < inputs.length; i++)
     {
-        inputs[i].addEventListener("keydown", process_input_modify, true);
-        inputs[i].addEventListener("keyup", refresh_total_reimbursement, true);
+        inputs[i].addEventListener("keydown", on_key_down, true);
+        inputs[i].addEventListener("keyup", on_key_up, true);
     }
     refresh_total_reimbursement();
 }
@@ -88,7 +82,7 @@ function save(save_button)
         {
             input_num = 0;
         }
-        query_string += inputs[i].id + "=" + parseInt(input_num)*100;
+        query_string += inputs[i].id + "=" + parseInt(parseFloat(input_num)*100);
         if (i < inputs.length-1)
         {
             query_string += "&";
@@ -118,92 +112,6 @@ function save_finished(data)
     }
 }
 
-function process_input_modify(event)
-{
-    var keycode = event.keyCode;
-    if (event.shiftKey)
-    {
-        event.preventDefault();
-        return;
-    }
-    if (keycode == 8 || keycode == 9) // backspace and tab are always allowed
-        return;
-    // Don't accept input that isn't
-    if ((keycode > 57 || keycode < 48)  // a number
-        && keycode != 190)              // a period
-    {
-        event.preventDefault();
-        return;
-    }
-    
-    var input_so_far = event.srcElement.value;
-    var input_type = event.srcElement.id;
-    var value_to_be = input_so_far + String.fromCharCode(keycode);
-    // Make sure the number
-    if(input_so_far.match(/\./g) && keycode === 190 ||                              // is valid
-       input_so_far.length == 0 && keycode === 48 ||                                // doesn't have leading zeros
-       parseFloat(value_to_be) > max_values[input_type] ||                          // is less than the allowed maximum
-       (value_to_be.indexOf(".") !== -1 && value_to_be.split(".")[1].length > 2))   // doesn't have precision greater than 2
-
-    {
-        event.preventDefault();
-        return;
-    }
-    
-}
-
-function field_mouseover(node)
-{
-    var to_fade_out = node.parentNode.getElementsByClassName("field-value");
-    for (var i = 0; i < to_fade_out.length; i++)
-    {
-        remove_class(to_fade_out[i], "fade-in");
-        add_class(to_fade_out[i],  "fade-out");
-    }
-    remove_class(node.parentNode, "no-overlay");
-    add_class(node.parentNode, "info-overlay");
-
-    var to_fade_in = node.parentNode.getElementsByClassName("detail");
-    for (var i = 0; i < to_fade_in.length; i++)
-    {
-        remove_class(to_fade_in[i], "fade-out");
-        add_class(to_fade_in[i], "fade-in");
-    }
-}
-
-function field_mouseout(event)
-{
-    var node  = event.toElement || event.relatedTarget;
-    if (node == null || node.parentNode == this || node == this)
-        return;
-    node = event.fromElement;
-    if (!has_class(node, "field"))
-    {
-        node = node.parentNode;
-    }
-    var value_nodes = node.getElementsByClassName('field-value');
-    for (var i = 0; i < value_nodes.length; i++)
-    {
-        remove_class(value_nodes[i], "fade-out");
-        add_class(value_nodes[i], "fade-in");
-    }
-    remove_class(node, "info-overlay");
-    add_class(node, "no-overlay");
-
-    var to_fade_out = node.getElementsByClassName("detail");
-    for (var i = 0; i < to_fade_out.length; i++)
-    {
-        remove_class(to_fade_out[i], "fade-in");
-        add_class(to_fade_out[i], "fade-out");
-    }
-}
-
-function remove_class(node, class_to_remove)
-{
-    var re = new RegExp("(?:^|\\s)" + class_to_remove + "(?!\\S)", "g");
-    node.className = node.className.replace(re, '');
-}
-
 function add_class(node, class_to_add)
 {
     var re = new RegExp("(?:^|\\s)" + class_to_add + "(?!\\S)", "g");
@@ -213,8 +121,49 @@ function add_class(node, class_to_add)
     }
 }
 
+function is_numeric(n)
+{
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+var before_key_press_field_value = null;
+function on_key_down(event)
+{
+    if (before_key_press_field_value != null)
+    {
+        event.preventDefault();
+    }
+    else
+    {
+        before_key_press_field_value = event.srcElement.value;
+    }
+}
+
+function on_key_up(event)
+{
+    var changed_input = event.srcElement.value;
+    if (changed_input === "")
+    {
+        changed_input = "0";
+    }
+    if (!is_numeric(changed_input) ||
+        max_values[event.srcElement.id] < changed_input ||
+        (changed_input.indexOf(".") !== -1 && changed_input.split(".")[1].length > 2))
+    {
+        event.srcElement.value = before_key_press_field_value;
+    }
+    before_key_press_field_value = null;
+    refresh_total_reimbursement();
+}
+
+function remove_class(node, class_to_remove)
+{
+    var re = new RegExp("(?:^|\\s)" + class_to_remove + "(?!\\S)", "g");
+    node.className = node.className.replace(re, '');
+}
+
 function has_class(node, class_to_check)
 {
-    var re = new RegExp("(?:^|\\s)" + class_to_check             + "(?!\\S)", "g");
+    var re = new RegExp("(?:^|\\s)" + class_to_check + "(?!\\S)", "g");
     return (node.className.match(re) != null);
 }
